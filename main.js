0294cc4c6bb13c0b35435828320a32147a99c237
brd3v <bnsiq2015@gmail.com>
2023-09-10 01:13:48 -0300

    finalizando css, e editando logica do botao com sintaxe errada ocasionando bug de clicar em qualqer lugar e avançar, e botão de voltar

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let animes = ['<img src="./img/Goku.png"></img>', '<img src="./img/pngegg.png"></img>', '<img src="./img/sakura.png"></img>', '<img src="./img/Shenlong.png"></img>', '<img src="./img/vegeta.png"></img>']
 let teste= document.getElementById('imagens')
-
-
-
-
 btn = document.getElementById('btn')
+btnx = document.getElementById('btnx')
 let i =0
-btn = addEventListener('click', function(e){
+
+
+
+
+
+btn.addEventListener('click', function(e){
     e.preventDefault()
     // Exibir o item atual do array
     teste.innerHTML = animes[i];
@@ -17,3 +19,15 @@ btn = addEventListener('click', function(e){
 
     
 })
+
+btnx.addEventListener('click', function(e){
+    e.preventDefault()
+
+    i = (i - 1 + animes.length) % animes.length
+    teste.innerHTML = animes[i]
+
+
+})
+btnx.addEventListener('mousedown', function (e) {
+    e.stopPropagation();
+});voltar
